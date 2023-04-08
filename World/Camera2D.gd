743369c02eb250c3b8fd640198d1a115cd70b0a4
Ishan Chawla <ishan.kpv@gmail.com>
2023-04-08 13:24:58 -0700

    zoom with +/-

---
+++ World/Camera2D.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+var zoom_level
+# Amount zoomed on each zoom event
+const ZOOM_SPEED = .25
+
+func _ready():
+	# Setup default zoom level
+	zoom_level = zoom[0]
+
+func _process(_delta):
+	if (Input.is_action_just_pressed("zoom_in")):
+		zoom_level += ZOOM_SPEED
+		update_zoom()
+	if (Input.is_action_just_pressed("zoom_out")):
+		zoom_level -= ZOOM_SPEED
+		update_zoom()
+
+# sync camera's zoom to the zoom_level
+func update_zoom():
+	set_zoom(Vector2(zoom_level, zoom_level))
